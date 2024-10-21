5512f8b3e74d54c634855a07969d4c915a4891e5
Brad Howes <bradhowes@mac.com>
2024-10-21 23:00:18 +0200

    New environment value

---
+++ Features/Sources/SwiftUISupport/VisibilityEditMode.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+private struct VisibilityEditMode: EnvironmentKey {
+  static let defaultValue: EditMode = .inactive
+}
+
+extension EnvironmentValues {
+  var visibilityEditMode: EditMode {
+    get { self[VisibilityEditMode.self] }
+    set { self[VisibilityEditMode.self] = newValue }
+  }
+}
+
+extension View {
+  public func visbilityEditModel(_ editMode: EditMode) -> some View {
+    environment(\.visibilityEditMode, editMode)
+  }
+}
