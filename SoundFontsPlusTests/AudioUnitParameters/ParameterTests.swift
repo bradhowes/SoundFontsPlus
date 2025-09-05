1aff9507e5ab70822ecdfba9fc63d06a997c76a7
Brad Howes <bradhowes@mac.com>
2025-09-05 18:17:37 +0200

    AudioUnitParameter tests

---
+++ SoundFontsPlusTests/AudioUnitParameters/ParameterTests.swift
@@ -0,0 +1,31 @@
+import Foundation
+import SnapshotTesting
+import SwiftUI
+import Testing
+
+@testable import SoundFontsPlus
+
+extension BaseSuite {
+
+  @Suite
+  struct ParameterTests {
+
+    @Test
+    func testTreeCreation() {
+      let tree = ParameterAddress.createParameterTree()
+      #expect(tree.children.count == ParameterAddress.count)
+    }
+
+    @Test func lookup() async throws {
+      let tree = ParameterAddress.createParameterTree()
+      #expect(tree[.delayEnabled].displayName == "Enabled")
+      #expect(tree[.delayTime].displayName == "Time")
+      #expect(tree[.delayFeedback].displayName == "Feedback")
+      #expect(tree[.delayCutoff].displayName == "Cutoff")
+      #expect(tree[.delayAmount].displayName == "Amount")
+      #expect(tree[.reverbEnabled].displayName == "Enabled")
+      #expect(tree[.reverbRoomIndex].displayName == "Room")
+      #expect(tree[.reverbAmount].displayName == "Amount")
+    }
+  }
+}
