a0e8a1370b2dfa536b3d8015e08d91f2b5108888
Brad Howes <bradhowes@mac.com>
2025-09-28 11:08:31 +0200

    Initial test

---
+++ SoundFontsPlusTests/Extensions/AVAudioSessionTests.swift
@@ -0,0 +1,24 @@
+import AVKit
+import Dependencies
+import Foundation
+import SQLiteData
+import Testing
+
+@testable import SoundFontsPlus
+
+extension BaseTestSuite {
+
+  @MainActor
+  struct AVAudioSessionTests {}
+}
+
+extension BaseTestSuite.AVAudioSessionTests {
+
+  @Test func monitorVolume() async throws {
+    // TODO: create AVAudioSession mock to properly test behavior
+    try? AVAudioSession.sharedInstance().setActive(true)
+    let value = AVAudioSession.sharedInstance().startObservingOutputVolume { _ in
+      print("terminated")
+    }
+  }
+}
