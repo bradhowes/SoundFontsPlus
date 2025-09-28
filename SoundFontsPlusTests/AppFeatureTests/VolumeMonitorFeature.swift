acbee867199c582a146192ab2be2a3e0f0406d32
Brad Howes <bradhowes@mac.com>
2025-09-28 11:11:43 +0200

    Formatting

--- SoundFontsPlusTests/AppFeatureTests/VolumeMonitorFeature.swift
+++ SoundFontsPlusTests/AppFeatureTests/VolumeMonitorFeature.swift
@@ -28,107 +28,114 @@ extension BaseTestSuite {
       self.volumes = volumes
       self.store = store
     }
+  }
+}
 
-    func togglePresetId(
-      assert updateStateToExpectedResult: ((_ state: inout VolumeMonitorFeature.State) throws -> Void)? = nil,
-    ) async {
-      @Shared(.activeState) var activeState
-      let newValue = activeState.activePresetId == nil ? Preset.ID(rawValue: 1) : nil
-      $activeState.activePresetId.withLock { $0 = newValue }
-      await store.send(.activePresetIdChanged(newValue), assert: updateStateToExpectedResult)
-    }
+extension BaseTestSuite.VolumeMonitorFeatureTests {
 
-    @Test
-    func volumeGoesToZero() async throws {
-      await store.send(.initialize)
-      volumes.advance()
-      await store.receive(\.volumeChanged) { $0.noVolumeReason = .volumeLevelIsZero }
-      await store.receive(\.delegate)
-      volumes.advance()
-      await store.receive(\.volumeChanged) { $0.noVolumeReason = nil }
-      await store.receive(\.delegate)
-      await store.send(.deinitialize)
-    }
+  func togglePresetId(
+    assert updateStateToExpectedResult: ((_ state: inout VolumeMonitorFeature.State) throws -> Void)? = nil,
+  ) async {
+    @Shared(.activeState) var activeState
+    let newValue = activeState.activePresetId == nil ? Preset.ID(rawValue: 1) : nil
+    $activeState.activePresetId.withLock { $0 = newValue }
+    await store.send(.activePresetIdChanged(newValue), assert: updateStateToExpectedResult)
+  }
 
-    @Test
-    func activePresetBecomesNil() async throws {
-      await store.send(.initialize)
-      await togglePresetId { $0.noVolumeReason = .noActivePreset }
-      await store.receive(\.delegate)
-      await togglePresetId { $0.noVolumeReason = nil }
-      await store.receive(\.delegate)
-      await store.send(.deinitialize)
-    }
+  @Test
+  func volumeGoesToZero() async throws {
+    await store.send(.initialize)
+    volumes.advance()
+    await store.receive(\.volumeChanged) { $0.noVolumeReason = .volumeLevelIsZero }
+    await store.receive(\.delegate)
+    volumes.advance()
+    await store.receive(\.volumeChanged) { $0.noVolumeReason = nil }
+    await store.receive(\.delegate)
+    await store.send(.deinitialize)
+  }
 
-    @Test
-    func ignorePresetChangesWhenVolumeIsZero() async throws {
-      await store.send(.initialize)
-      volumes.advance()
-      await store.receive(\.volumeChanged) { $0.noVolumeReason = .volumeLevelIsZero }
-      await store.receive(\.delegate)
-      await togglePresetId()
-      await togglePresetId()
-      volumes.advance()
-      await store.receive(\.volumeChanged) { $0.noVolumeReason = nil }
-      await store.receive(\.delegate)
-      await store.send(.deinitialize)
-    }
+  @Test
+  func activePresetBecomesNil() async throws {
+    await store.send(.initialize)
+    await togglePresetId { $0.noVolumeReason = .noActivePreset }
+    await store.receive(\.delegate)
+    await togglePresetId { $0.noVolumeReason = nil }
+    await store.receive(\.delegate)
+    await store.send(.deinitialize)
+  }
 
-    @Test
-    func ignoreVolumeChangesWhenPresetIsNil() async throws {
-      await store.send(.initialize)
-      @Shared(.activeState) var activeState
-      await togglePresetId { $0.noVolumeReason = .noActivePreset }
-      await store.receive(\.delegate)
-      volumes.advance()
-      await store.receive(\.volumeChanged)
-      volumes.advance()
-      await store.receive(\.volumeChanged)
-      await togglePresetId { $0.noVolumeReason = nil }
-      await store.receive(\.delegate)
-      await store.send(.deinitialize)
-    }
+  @Test
+  func ignorePresetChangesWhenVolumeIsZero() async throws {
+    await store.send(.initialize)
+    volumes.advance()
+    await store.receive(\.volumeChanged) { $0.noVolumeReason = .volumeLevelIsZero }
+    await store.receive(\.delegate)
+    await togglePresetId()
+    await togglePresetId()
+    volumes.advance()
+    await store.receive(\.volumeChanged) { $0.noVolumeReason = nil }
+    await store.receive(\.delegate)
+    await store.send(.deinitialize)
+  }
 
-    @Test
-    func switchReasonWhenNecessary() async throws {
-      await store.send(.initialize)
-      await togglePresetId { $0.noVolumeReason = .noActivePreset }
-      await store.receive(\.delegate)
-      volumes.advance()
-      await store.receive(\.volumeChanged)
-      await togglePresetId { $0.noVolumeReason = .volumeLevelIsZero }
-      await store.receive(\.delegate)
-      await togglePresetId()
-      volumes.advance()
-      await store.receive(\.volumeChanged) { $0.noVolumeReason = .noActivePreset }
-      await store.receive(\.delegate)
-      await store.send(.deinitialize)
-    }
+  @Test
+  func ignoreVolumeChangesWhenPresetIsNil() async throws {
+    await store.send(.initialize)
+    @Shared(.activeState) var activeState
+    await togglePresetId { $0.noVolumeReason = .noActivePreset }
+    await store.receive(\.delegate)
+    volumes.advance()
+    await store.receive(\.volumeChanged)
+    volumes.advance()
+    await store.receive(\.volumeChanged)
+    await togglePresetId { $0.noVolumeReason = nil }
+    await store.receive(\.delegate)
+    await store.send(.deinitialize)
+  }
 
-    @Test
-    @MainActor
-    func volumeMonitorPreview() async throws {
-      let volumes = OutputVolumeFlipFlop()
-      // swiftlint:disable:next redundant_discardable_let
-      let _ = prepareDependencies { $0.outputVolume = volumes.outputVolume() }
-      let store: StoreOf<VolumeMonitorFeature> = .init(
-        initialState: VolumeMonitorFeature.State(
-          noVolumeReason: .volumeLevelIsZero)) {
-        VolumeMonitorFeature()
-      }
-      let view = VolumeMonitorDemoView(volumes: volumes, store: store)
+  @Test
+  func switchReasonWhenNecessary() async throws {
+    await store.send(.initialize)
+    await togglePresetId { $0.noVolumeReason = .noActivePreset }
+    await store.receive(\.delegate)
+    volumes.advance()
+    await store.receive(\.volumeChanged)
+    await togglePresetId { $0.noVolumeReason = .volumeLevelIsZero }
+    await store.receive(\.delegate)
+    await togglePresetId()
+    volumes.advance()
+    await store.receive(\.volumeChanged) { $0.noVolumeReason = .noActivePreset }
+    await store.receive(\.delegate)
+    await store.send(.deinitialize)
+  }
 
-      if BaseTestSuite.isLocal {
-        withSnapshotTesting(record: .failed) {
-          assertSnapshot(
-            of: view,
-            as: .image(
-              layout: .fixed(width: 400, height: 800),
-              traits: .init(userInterfaceStyle: .dark)
-            )
-          )
+  @Test
+  func processReason() {
+    // TODO: make ProgressHUD a dependency to actually test the mapping
+    VolumeMonitorModifier.processReason(.noActivePreset)
+    VolumeMonitorModifier.processReason(.volumeLevelIsZero)
+    VolumeMonitorModifier.processReason(nil)
+  }
+
+  @Test
+  @MainActor
+  func volumeMonitorPreview() async throws {
+    let volumes = OutputVolumeFlipFlop()
+    // swiftlint:disable:next redundant_discardable_let
+    let _ = prepareDependencies { $0.outputVolume = volumes.outputVolume() }
+    let store: StoreOf<VolumeMonitorFeature> = .init(
+      initialState: VolumeMonitorFeature.State(
+        noVolumeReason: .volumeLevelIsZero)) {
+          VolumeMonitorFeature()
         }
-      }
+    let view = VolumeMonitorDemoView(volumes: volumes, store: store)
+
+    try withSnapshotTesting(record: .failed) {
+      try BaseTestSuite.assertSnap(
+        matching: view,
+        size: .init(width: 400, height: 800),
+        colorScheme: .dark
+      )
     }
   }
 }
