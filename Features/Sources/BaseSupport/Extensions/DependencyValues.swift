2670be90ea1a46d0b6b2acd05781b519a425364c
Brad Howes <bradhowes@mac.com>
2025-10-08 11:52:30 +0200

    Slight cleanup

--- Features/Sources/BaseSupport/Extensions/DependencyValues.swift
+++ Features/Sources/BaseSupport/Extensions/DependencyValues.swift
@@ -1,62 +1,7 @@
 // Copyright © 2025 Brad Howes. All rights reserved.
 
-import AudioUnit.AUParameters
-import AVFAudio.AVAudioSession
 import Dependencies
-import DependenciesMacros
 
-@DependencyClient
-public struct AudioSession: Sendable {
-  public let setCategory: @Sendable (
-    AVAudioSession.Category,
-    _ mode: AVAudioSession.Mode,
-    _ options: AVAudioSession.CategoryOptions
-  ) throws -> Void
-  public let sampleRate: @Sendable () -> Double
-  public let setPreferredSampleRate: @Sendable (Double) throws -> Void
-  public let setPreferredIOBufferDuration: @Sendable (Double) throws -> Void
-  public let currentRoute: @Sendable () -> AVAudioSessionRouteDescription
-  public let setActive: @Sendable (Bool, _ options: AVAudioSession.SetActiveOptions) throws -> Void
-
-  public init(
-    setCategory: @escaping @Sendable (AVAudioSession.Category, AVAudioSession.Mode, AVAudioSession.CategoryOptions) throws -> Void = { _, _, _ in unimplemented() },
-    sampleRate: @escaping @Sendable () -> Double = { unimplemented(); return 0.0 },
-    setPreferredSampleRate: @escaping @Sendable (Double) throws -> Void = { _ in unimplemented() },
-    setPreferredIOBufferDuration: @escaping @Sendable (Double) throws -> Void = { _ in unimplemented() },
-    currentRoute: @escaping @Sendable () -> AVAudioSessionRouteDescription = { unimplemented(); return .init() },
-    setActive: @escaping @Sendable (Bool, _ options: AVAudioSession.SetActiveOptions) throws -> Void = { _, _ in unimplemented() }
-  ) {
-    self.setCategory = setCategory
-    self.sampleRate = sampleRate
-    self.setPreferredSampleRate = setPreferredSampleRate
-    self.setPreferredIOBufferDuration = setPreferredIOBufferDuration
-    self.currentRoute = currentRoute
-    self.setActive = setActive
-  }
-}
-
-extension AudioSession: DependencyKey {
-  public static var liveValue: AudioSession {
-    let session: @Sendable () -> AVAudioSession = { AVAudioSession.sharedInstance() }
-    return .init(
-      setCategory: { try session().setCategory($0, mode: $1, options: $2) },
-      sampleRate: { session().sampleRate },
-      setPreferredSampleRate: { try session().setPreferredSampleRate($0) },
-      setPreferredIOBufferDuration: { try session().setPreferredIOBufferDuration($0) },
-      currentRoute: { session().currentRoute },
-      setActive: { try session().setActive($0, options: $1) }
-    )
-  }
-}
-
-//extension AUParameterTree: @retroactive TestDependencyKey {}
-//
-//extension AUParameterTree: @retroactive DependencyKey {
-//  public static var liveValue: AUParameterTree { ParameterAddress.createParameterTree() }
-//  public static var previewValue: AUParameterTree { ParameterAddress.createParameterTree() }
-//  public static var testValue: AUParameterTree { ParameterAddress.createParameterTree() }
-//}
-//
 extension DependencyValues {
 
   public var audioSession: AudioSession {
@@ -64,11 +9,6 @@ extension DependencyValues {
     set { self[AudioSession.self] = newValue }
   }
 
-//  public var parameters: AUParameterTree {
-//    get { self[AUParameterTree.self] }
-//    set { self[AUParameterTree.self] = newValue }
-//  }
-//
   public var fileManager: FileManagerClient {
     get { self[FileManagerClient.self] }
     set { self[FileManagerClient.self] = newValue }
