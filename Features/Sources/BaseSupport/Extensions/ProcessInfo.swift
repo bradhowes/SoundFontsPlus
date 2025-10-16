769ea3c4d000815c04d06dc4cae8c3cc33fd7207
Brad Howes <bradhowes@mac.com>
2025-10-16 16:48:39 +0200

    Use Github-specific environment variable

--- Features/Sources/BaseSupport/Extensions/ProcessInfo.swift
+++ Features/Sources/BaseSupport/Extensions/ProcessInfo.swift
@@ -3,5 +3,5 @@
 import Foundation
 
 extension ProcessInfo {
-  public var isOnGithub: Bool { environment["HOME"]?.starts(with: "/Users/runner/") ?? true }
+  public var isOnGithub: Bool { !(environment["SNAPSHOT_ARTIFACTS"]?.isEmpty ?? true) }
 }
