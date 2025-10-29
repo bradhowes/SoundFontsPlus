6b5d73d35515ae22f6c48961471baf9b9881ee3c
Brad Howes <bradhowes@mac.com>
2025-10-29 15:27:16 +0100

    Fix missing test file and clean up.

---
+++ Features/Tests/FileImporterTests/FileImporterTests.swift
@@ -0,0 +1,28 @@
+// Copyright © 2025 Brad Howes. All rights reserved.
+
+import DependenciesTestSupport
+import FeatureSupport
+import SnapshotTesting
+import Testing
+import TestSupport
+
+@testable import FileImporter
+
+@Suite(
+//  .dependencies {
+//    $0.defaultDatabase = try appDatabase()
+//  }
+)
+@MainActor
+struct FileImporterTests {
+
+  func store() -> TestStoreOf<FileImporter> {
+    TestStoreOf<FileImporter>(initialState: .init()) {
+      FileImporter()
+    }
+  }
+
+  @Test func firstTest() async {
+    let store = store()
+  }
+}
