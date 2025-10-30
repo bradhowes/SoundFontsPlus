5ac94f786f0dc4e66764ae848b6733b9d9d7394a
Brad Howes <bradhowes@mac.com>
2025-10-30 09:18:22 +0100

    Added test target for app

---
+++ SoundFontsPlusTests/SoundFontsPlusTests.swift
@@ -0,0 +1,16 @@
+//
+//  SoundFontsPlusTests.swift
+//  SoundFontsPlusTests
+//
+//  Created by Brad Howes on 10/30/25.
+//
+
+import Testing
+
+struct SoundFontsPlusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
