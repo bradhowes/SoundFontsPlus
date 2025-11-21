48eec1a37986bf0043f3ef95b8920e8b813e1e73
Brad Howes <bradhowes@mac.com>
2025-11-21 16:49:15 +0100

    Add commentary and simplify execution

--- Features/Sources/BuildFluidFontCmd/main.swift
+++ Features/Sources/BuildFluidFontCmd/main.swift
@@ -1,7 +1,8 @@
 // Copyright © 2025 Brad Howes. All rights reserved.
 
 /**
- Custom script to create the `FluidR3_GM.sf2` soundfont file from the given N slices
+ Custom script to create the `FluidR3_GM.sf2` soundfont file from 3 smaller parts, just to get out from using git-lfs
+ on this repo.
 
  First argument is the path to this script. Last argument is the output file to generate.
  The remaining arguments are the pieces to concatenate to make the output file.
@@ -11,32 +12,27 @@ import Foundation
 
 let arguments = ProcessInfo().arguments
 if arguments.count < 3 {
-  print("missing arguments - \(arguments)")
+  print("BuldFluidFontCmd: missing arguments - \(arguments)")
   exit(1)
 }
 
-let output = URL(fileURLWithPath: arguments.last!)
+let first = URL(fileURLWithPath: arguments[1], isDirectory: false)
+let output = URL(fileURLWithPath: arguments.last!, isDirectory: false)
+
 if FileManager.default.fileExists(atPath: output.path()) {
-  print("skipping -- \(output) already exists")
+  print("BuldFluidFontCmd: skipping -- \(output.path()) already exists")
   exit(0)
 }
 
-for input in arguments.dropFirst().dropLast() {
-  try! Data(contentsOf: .init(filePath: input, directoryHint: .notDirectory)).appendTo(output)
-}
+print("BuldFluidFontCmd: generating output - \(output)")
 
-extension Data {
+// Copy first part so we can get a `FileHandle` and then use that to append the others.
+try FileManager.default.copyItem(at: first, to: output)
 
-  func appendTo(_ fileURL: URL) throws {
-    if let fileHandle = FileHandle(forWritingAtPath: fileURL.path) {
-      defer {
-        fileHandle.closeFile()
-      }
-      fileHandle.seekToEndOfFile()
-      fileHandle.write(self)
-    }
-    else {
-      try write(to: fileURL, options: .atomic)
-    }
-  }
+let fileHandle = FileHandle(forWritingAtPath: output.path())!
+defer { fileHandle.closeFile() }
+try fileHandle.seekToEnd()
+
+for input in arguments[2..<arguments.count] {
+  try fileHandle.write(contentsOf: Data(contentsOf: .init(filePath: input, directoryHint: .notDirectory)))
 }
