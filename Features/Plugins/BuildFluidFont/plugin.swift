765d724636b7bd02c26a8206981663ba8b7808b6
Brad Howes <bradhowes@mac.com>
2025-11-21 16:48:24 +0100

    Add commentary

--- Features/Plugins/BuildFluidFont/plugin.swift
+++ Features/Plugins/BuildFluidFont/plugin.swift
@@ -12,27 +12,49 @@ struct BuildFluidFont: BuildToolPlugin {
     context: PackagePlugin.PluginContext,
     target: any PackagePlugin.Target
   ) async throws -> [PackagePlugin.Command] {
-    let input = target
+
+    // Create file URLs to the parts to concatenate to create the final file. The `target` is a Swift Package target
+    // which has a `plugins:["BuildFluidFont"]` value in its definition, like:
+    //
+    // .target(
+    //   name: "SF2Resources",
+    //   dependencies: [
+    //     .product(name: "Engine", package: "SF2Lib"),
+    //   ],
+    //   resources: [.process("Resources")],
+    //   plugins: ["BuildFluidFont"]
+    // ),
+    print("target.directoryURL: \(target.directoryURL)")
+    let inputDirectory = target
       .directoryURL
-      .deletingLastPathComponent()
-      .appending(path: "SF2Resources/FluidR3_GM_Parts", directoryHint: .isDirectory)
+      .appending(path: "FluidR3_GM_Parts", directoryHint: .isDirectory)
     let inputs: [URL] = (1...3).map {
       .init(
         filePath: "FluidR3_GM.sf2.\($0)",
         directoryHint: .notDirectory,
-        relativeTo: input
+        relativeTo: inputDirectory
       )
     }
+    print("inputs: \(inputs)")
+
+    // Create output destination file URL in the plugin's work directory. This acts as a a cache of the build and will
+    // only be regenerated if a dependency changes. The build system will actually perform an additional copy of this
+    // file into the target's bundle when it generates the target.
     let output = context
       .pluginWorkDirectoryURL
       .appendingPathComponent("FluidR3_GM.sf2")
-    return [
-      .buildCommand(
+    print("output: \(output)")
+
+    // Finally, create a Swift command to generate the output from the inputs if necessary during a build.
+    let commands = [
+      Command.buildCommand(
         displayName: "Generating FluidR3_GM file from parts",
         executable: try context.tool(named: "BuildFluidFontCmd").url,
         arguments: inputs.map(\.path) + [output.path()],
         inputFiles: inputs,
         outputFiles: [output])
     ]
+    print("commands: \(commands)")
+    return commands
   }
 }
