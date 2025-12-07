3c0617330a9cf6aa7508e8ce0822ed3e0aea3796
Brad Howes <bradhowes@mac.com>
2025-12-07 12:01:36 +0100

    Use embedded AudioUnit.

--- Features/Sources/BaseSupport/SynthAUv3ComponentDescription.swift
+++ Features/Sources/BaseSupport/SynthAUv3ComponentDescription.swift
@@ -9,8 +9,11 @@ public struct SynthAUv3ComponentDescription: TestDependencyKey {
     let componentType = FourCharCode(stringLiteral: bundle.string(forKey: "AU_COMPONENT_TYPE"))
     // NOTE: do not use AU_COMPONENT_SUBTYPE until the AUv3 plugin is ready to use. Until then, need to point to
     // something that is unique.
-    let componentSubtype = FourCharCode(stringLiteral: "Sf2L") // bundle.string(forKey: "AU_COMPONENT_SUBTYPE"))
+
+    // let componentSubtype = FourCharCode(stringLiteral: "Sf2L")
+    let componentSubtype = FourCharCode(stringLiteral: bundle.string(forKey: "AU_COMPONENT_SUBTYPE"))
     let componentManufacturer = FourCharCode(stringLiteral: bundle.string(forKey: "AU_COMPONENT_MANUFACTURER"))
+
     precondition(
       componentType != .invalidFourCharCode &&
       componentSubtype != .invalidFourCharCode &&
